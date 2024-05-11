75e79a87c11cd71f76956d71f5d17a81733f3986
Vamshikrishna Enjapuri <vamsikrishnaenjapuri@gmail.com>
2024-05-11 18:50:14 +0530

    Minor changes

--- src/config/enpoints/index.jsx
+++ src/config/enpoints/index.jsx
@@ -1,21 +1,14 @@
 export const USER_ENDPOINTS = {
-    agentdata: `/v1/agents`,
-    getUsers: `/v1/users`,
-    getCustomer: `/v1/users`,
-    getKnowledge:`/v1/knowledge_base`,
-    getvoicecall:`/v1/voicecalls/`,
-    getaction:`/v1/actions/`,
-    getassist:`/v1/ai/assistants`,
-    login:`/v1/login`,
-    makecall:`/v1/voicecalls`,
-    createChat:`/v1/ai/chat`,
-    addIntent:`/v1/voiceassistants`,
-    getmodel:`/v1/ai/models`
-
-   
-    
-   
-
-   
-
-  };
+  agentdata: `/v1/agents`,
+  getUsers: `/v1/users`,
+  getCustomer: `/v1/users`,
+  getKnowledge: `/v1/knowledge_base`,
+  getvoicecall: `/v1/voicecalls/`,
+  getaction: `/v1/actions/`,
+  getassist: `/v1/voiceassistants`,
+  login: `/v1/login`,
+  makecall: `/v1/voicecalls`,
+  createChat: `/v1/ai/chat`,
+  addIntent: `/v1/voiceassistants`,
+  getmodel: `/v1/ai/models`,
+};
